98919ac5cca110d295020bec151cff913a238bca
tristanbarlowg@gmail.com <Samandfrodo12>
2018-04-07 17:52:15 +0100

    Animals now spawn in animated. Had to do a lot of scaling

---
+++ Pocket Pals App 1/Assets/Scripts/PocketPalParent.cs
@@ -0,0 +1,49 @@
+﻿/**
+ * Author Tristan Barlow, Rich Steele
+ **/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PocketPalParent : MonoBehaviour {
+
+    //this will be the ID for each pocketpal this should be unique to each pocketpal and used to identify it for the inventory system. 
+    public int PocketPalID = 0;
+
+    public Vector3 Rotation = new Vector3(0, 15, 0);
+
+    public Vector3 Scale = new Vector3(1, 1, 1);
+
+    public Vector3 Offset = new Vector3(0, 0, 0);
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+        transform.position += Offset;
+        //Some of the models are very small this will allow you to scale them
+        transform.localScale = Vector3.Scale(transform.localScale, Scale);
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // rotates the object on the x, y and z axis
+        transform.Rotate(Rotation * Time.deltaTime); 
+    }
+
+    // OnMouseDown also works as a touch on an iphone or other mobile device
+    void OnMouseDown()
+    {
+        //do relevant coding to add to the inventory etc.
+
+        // Removes Pocket Pal when clicked or touched
+        Destroy(gameObject);   
+
+        // Loads the Spotted Scene when called
+        SceneManager.LoadSceneAsync(2);        
+    }
+}
--- Pocket Pals App 1/Assets/Scripts/PocketPalSpawnManager.cs
+++ Pocket Pals App 1/Assets/Scripts/PocketPalSpawnManager.cs
@@ -5,15 +5,24 @@
 
 public class PocketPalSpawnManager : MonoBehaviour
 {
-	public GameObject PocketPal;                 // The Pocket Pal prefab to be spawned
-	public float spawnTime = 1f;                 // How long between each spawn
+	public GameObject[] PocketPals;                 // The Pocket Pal prefab to be spawned
+	public float spawnTime = 2f;                 // How long between each spawn
 	public Transform[] spawnPoints;              // An array of spawn points Pocket Pals can spawn from. There are 20
 
-	private int maxPocketPals = 2;               // The max number of Pocket Pals that can spawn at any one time
+	public int maxPocketPals = 10;               // The max number of Pocket Pals that can spawn at any one time
 	private int currentPocketPals = 0;           // The current number of Pocket Pals that have spawned at one time
 
 	void Start ()
 	{
+        //Iter throught the PocketPalscripts and set their IDs
+        int iter = 0;
+        foreach (GameObject o in PocketPals)
+        {
+            iter++;
+            PocketPalParent p = o.GetComponent(typeof(PocketPalParent)) as PocketPalParent;
+            p.PocketPalID = iter;
+        }
+
 		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time
 		InvokeRepeating ("Spawn", spawnTime, spawnTime);
 	}
@@ -28,9 +37,10 @@ public class PocketPalSpawnManager : MonoBehaviour
 
 		// Find a random index between zero and one less than the number of spawn points
 		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        int RandomPocketPal = Random.Range(0, PocketPals.Length);
 
-		// Create an instance of the prefab at the randomly selected spawn point's position and rotation
-		Instantiate (PocketPal, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        // Create an instance of the prefab at the randomly selected spawn point's position and rotation
+        Instantiate (PocketPals[RandomPocketPal], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
 		currentPocketPals++;                      // Increases the currentPocketPals value by 1
 	}
 }
