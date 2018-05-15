b6a72021ba43f1253a280432870f7f94c20db956
tristanbarlow <tristanbarlowg@gmail.com>
2018-05-15 19:10:16 +0100

    Updated movement, currently not working quite as intended Should be much smoother. Removed useless stuff Added debug menu option

---
+++ Pocket Pals App 1/Assets/Scripts/DebugToggler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugToggler : MonoBehaviour {
+
+    public List<GameObject> DebugStuff;
+    private bool IsDebug = false;
+
+    public void ToggleDebug()
+    {
+        //flip flop for the toggle
+        if (IsDebug) IsDebug = false;
+        else IsDebug = true;
+
+        //loop through and set activeness
+        foreach (GameObject obj in DebugStuff)
+        {
+            obj.SetActive(IsDebug);
+        }
+    }
+}
