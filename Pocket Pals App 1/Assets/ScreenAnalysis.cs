67ed0ccd0cea94d1b914bf529e4e7513e394fcdd
Richard Steele <richvon2208@googlemail.com>
2018-05-18 13:21:56 +0100

    Screen analysis functions in. Should be good to apply

--- Pocket Pals App 1/Assets/ScreenAnalysis.cs
+++ Pocket Pals App 1/Assets/ScreenAnalysis.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class ScreenAnalysis : MonoBehaviour {
 
 	public RenderTexture cameraRender;
+	public Camera cameraObject;
 
 	public int NSamples = 10;
 
-	Texture2D renderTexture;
+	Texture2D cameraTexture = new Texture2D(0, 0);
+
+	// When checking for water around a blue pixel, this is far it will look
+	int waterSampleDistance = 20;
+
+	GameObject[] landTypes = new GameObject[]{};
+	GameObject[] forestTypes = new GameObject[]{};
+	GameObject[] urbanTypes = new GameObject[]{};
+	GameObject[] seaTypes = new GameObject[]{};
+	GameObject[] riverTypes = new GameObject[]{};
+	GameObject[] waterTypes = new GameObject[]{};
+
 
 	// Use this for initialization
 	void Start () {
 
 		// Set the texture2d to be the same size as the renderTexture
-		renderTexture.Resize (cameraRender.width, cameraRender.height);
+		cameraTexture.Resize (cameraRender.width, cameraRender.height);
 	}
 	
 	// Update is called once per frame
@@ -25,16 +37,20 @@ public class ScreenAnalysis : MonoBehaviour {
 	void UpdateTexture () {
 		
 		// Copy the renderTexture into the texture2d
-		renderTexture.ReadPixels (new Rect (0.0f, 0.0f, cameraRender.width, cameraRender.height), 0, 0);
+		cameraTexture.ReadPixels (new Rect (0.0f, 0.0f, cameraRender.width, cameraRender.height), 0, 0);
 
 		// Applies the read pixels to the texture
-		renderTexture.Apply ();
+		cameraTexture.Apply ();
 	}
 
-	void AnalyseScreen () {
+	// Will this ever be needed? Whole screen analysis as opposed to single pixel analysis
+	GameObject[] AnalyseScreen () {
+
+		// Update the cameraTexture to the current camera view image
+		UpdateTexture ();
 		
 		// Get all pixels from the texture
-		Color[] screenPixels = renderTexture.GetPixels ();
+		Color[] screenPixels = cameraTexture.GetPixels ();
 
 		// Data structure to fill with rgb colour values
 		Vector3 accumColour = new Vector4();
@@ -52,9 +68,75 @@ public class ScreenAnalysis : MonoBehaviour {
 		Vector3 averageColour = accumColour / NSamples;
 
 		// ToDo: do checks on this average colour to see what environment fills the screen
+		return GetAppropriatePalListFromMapColour(averageColour);
 	}
 
-	void AnalyseSpawnLocation (Vector3 worldSpawnLocation) {
+	GameObject[] AnalyseSpawnLocation (Vector3 worldSpawnLocation) {
 		// Analyse the pixel colour at the world spawn location to decide which land sea etc pocket pal list to spawn from
+
+		// World to screen location returns the pixel coordinates
+		Vector3 screenLocation = cameraObject.WorldToScreenPoint (worldSpawnLocation);
+
+		Color pixelColour = cameraTexture.GetPixel ((int)screenLocation.x, (int)screenLocation.y);
+
+		float r = pixelColour.r;
+		float g = pixelColour.g;
+		float b = pixelColour.b;
+
+		// Check for green
+		if (r < 0.2f && g > 0.8f && b < 0.2f) return forestTypes;
+
+		// Check for urban (grey rgb have similar values)
+		if (r - g < 0.2f && r - b < 0.2f
+			// Also check not pure white or black
+			&& r < 0.8f && r > 0.2f) return urbanTypes;
+
+		// Check for blue
+		if (r < 0.2f && g < 0.2f && b > 0.8f) {
+
+			// Need to check if sea or inland
+			// Will need a null check here or some type of off screen
+			Color leftSample = cameraTexture.GetPixel ((int)screenLocation.x - waterSampleDistance, (int)screenLocation.y);
+			Color rightSample = cameraTexture.GetPixel ((int)screenLocation.x + waterSampleDistance, (int)screenLocation.y);
+			Color upSample = cameraTexture.GetPixel ((int)screenLocation.x, (int)screenLocation.y + waterSampleDistance);
+			Color downSample = cameraTexture.GetPixel ((int)screenLocation.x, (int)screenLocation.y - waterSampleDistance);
+			
+			// If inland then pixels on either side will be land
+			if (!isBlue (leftSample) && !isBlue (rightSample)) return riverTypes;
+			if (!isBlue (upSample) && !isBlue (downSample)) return riverTypes;
+
+			// Else is sea 
+			return seaTypes;
+		}
+
+		// else return default land types
+		return landTypes;
+	}
+
+	bool isBlue(Color colour) {
+		if (colour.r < 0.2f && colour.g < 0.2f && colour.b > 0.8f) {
+			return true;
+		}
+		return false;
+	}
+
+	GameObject[] GetAppropriatePalListFromMapColour(Vector3 colour) {
+		float r = colour.x;
+		float g = colour.y;
+		float b = colour.z;
+
+		// Check for green
+		if (r < 0.2f && g > 0.8f && b < 0.2f) return forestTypes;
+
+		// Check for urban (grey rgb have similar values)
+		if (r - g < 0.2f && r - b < 0.2f
+			// Also check not pure white or black
+			&& r < 0.8f && r > 0.2f) return urbanTypes;
+
+		// Check for blue
+		if (r < 0.2f && g < 0.2f && b > 0.8f) return waterTypes;
+
+		// else return default land types
+		return landTypes;
 	}
 }
