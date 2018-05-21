849879d2263b4b3d435c273996b7f9ba4d459b9a
Richard Steele <richvon2208@googlemail.com>
2018-05-21 12:40:36 +0100

    Refactor of touch events

--- Pocket Pals App 1/Assets/Scripts/CameraController.cs
+++ Pocket Pals App 1/Assets/Scripts/CameraController.cs
@@ -8,11 +8,12 @@ public class CameraController : MonoBehaviour {
 	public float minimumCameraDistance = 4.0f;
 	public float maximumCameraDistance = 15.0f;
 	float currentCameraDistance;
+	bool IsDebug = false;
 
 	// The look at position for the camera added to the player position. Roughly shoulder or head position
 	Vector3 lookAtPositionPlayerOffset = new Vector3(0.0f, 2.0f, 0.0f);
 
-	// The player gameObject from which to get the player's location from
+	// The player gameObject from which to get the player's location and inventory from
 	public GameObject player;
 
 	// The ui compass that will rotate with the view rotation
@@ -21,8 +22,12 @@ public class CameraController : MonoBehaviour {
 	// The camera for which to use the world to screen location to determine swipe map rotation direction
 	public Camera gameCamera;
 
+	// The max distance allowed by the raycast hit detection
+	public float maxCaptureDistance = 10.0f;
+
 	Touch touchZero;
 	Touch touchOne;
+	Vector3 playerPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -34,107 +39,141 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        // Get the position of the player
-        Vector3 playerPosition = player.transform.position;
+		// Switch statement behaves differently with different number of touches
+		switch (Input.touchCount) { 
+
+		// Check for pinch to zoom control. The only control to use > two touches. Uses fallthrough cases
+		case 4:
+		case 3:
+		case 2:
+			PinchZoom ();
+			break;
+
+		// check for a single touch swiping
+		case 1:
+			RotateMap ();
+			CheckForTap ();
+			break;
+		}
+	}
+
+	void PinchZoom() {
+
+		// Get the position of the player
+		playerPosition = player.transform.position;
 
 		// Get the distance of the camera from the player at the start of this frame
 		currentCameraDistance = (transform.position - playerPosition).magnitude;
 
-		// Maybe swap all to a switch statement? Zero all variables on default case.
-		// Tidier, but does not easily allow for >2 touches doing pinch zooming. Using fallthrough cases
-		switch (Input.touchCount) { 
+		// Just store the first two touches, two is all we need
+		touchZero = Input.GetTouch (0);
+		touchOne = Input.GetTouch (1);
 
-		// Check for pinch to zoom control. The only control to use > two touches
-		case 4:
-		case 3:
-		case 2:
-			// Just store the first two touches, two is all we need
-			touchZero = Input.GetTouch (0);
-			touchOne = Input.GetTouch (1);
+		// Find the position in the previous frame of each touch
+		Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+		Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-			// Find the position in the previous frame of each touch
-			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+		// Find the magnitude of the vector (the distance) between the touches in each frame
+		// Previous frame
+		float prevTouchDelta = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+		// Current frame
+		float currentTouchDelta = (touchZero.position - touchOne.position).magnitude;
 
-			// Find the magnitude of the vector (the distance) between the touches in each frame
-			// Previous frame
-			float prevTouchDelta = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-			// Current frame
-			float currentTouchDelta = (touchZero.position - touchOne.position).magnitude;
+		// Find the difference in the distances between each frame
+		float deltaTouchDifference = prevTouchDelta - currentTouchDelta;
 
-			// Find the difference in the distances between each frame
-			float deltaTouchDifference = prevTouchDelta - currentTouchDelta;
+		// Adjust for different device's screen pixel density
+		deltaTouchDifference = 1 + deltaTouchDifference / Screen.width;
 
-			// Adjust for different device's screen pixel density
-			deltaTouchDifference = 1 + deltaTouchDifference / Screen.width;
+		// Apply the modifier to the current camera distance
+		currentCameraDistance *= deltaTouchDifference;
 
-			// Apply the modifier to the current camera distance
-			currentCameraDistance *= deltaTouchDifference;
+		// Clamp between min and max allowed values
+		currentCameraDistance = Mathf.Clamp (currentCameraDistance, minimumCameraDistance, maximumCameraDistance);
 
-			// Clamp between min and max allowed values
-			currentCameraDistance = Mathf.Clamp (currentCameraDistance, minimumCameraDistance, maximumCameraDistance);
+		// Calculate the new position of the camera
+		// Get the current camera vector
+		Vector3 currentCameraVector = (transform.position - playerPosition).normalized;
 
-			// Calculate the new position of the camera
-			// Get the current camera vector
-			Vector3 currentCameraVector = (transform.position - playerPosition).normalized;
+		// Set the new camera position from the player position + the camera vector * the new distance
+		transform.position = playerPosition + currentCameraVector * currentCameraDistance;
 
-			// Set the new camera position from the player position + the camera vector * the new distance
-			transform.position = playerPosition + currentCameraVector * currentCameraDistance;
+		// Set the transform rotation to look at the player + the look at position offset
+		transform.LookAt (playerPosition + lookAtPositionPlayerOffset);
+	}
 
-			// Set the transform rotation to look at the player + the look at position offset
-			transform.LookAt (playerPosition + lookAtPositionPlayerOffset);
+	void RotateMap() {
 
-			break;
+		// Get the position of the player
+		playerPosition = player.transform.position;
 
-			// check for a single touch swiping. Just left to right for now?
-		case 1:
-			// Store the touch
-			touchZero = Input.GetTouch (0);
+		// Store the touch
+		touchZero = Input.GetTouch (0);
 
-			// Get the horixontal component of the touch's movement
-			float deltaTouchX = touchZero.deltaPosition.x;
+		// Get the horixontal component of the touch's movement
+		float deltaTouchX = touchZero.deltaPosition.x;
 
-			// rotation direction reversed if touch is below the player figure
-			if (touchZero.position.y < gameCamera.WorldToScreenPoint (playerPosition).y)
-				
-				// Adjust for different device's screen widths. Then multiply by the angles that a total screen width swipe will rotate
-				deltaTouchX = deltaTouchX / Screen.width * 360;
-			else
-				deltaTouchX = -deltaTouchX / Screen.width * 360;
+		// rotation direction reversed if touch is below the player figure
+		if (touchZero.position.y < gameCamera.WorldToScreenPoint (playerPosition).y)
 
-			// Repeat for deltaY touch to enable circling movements to circle the map around
-			float deltaTouchY = touchZero.deltaPosition.y;
+			// Adjust for different device's screen widths. Then multiply by the angles that a total screen width swipe will rotate
+			deltaTouchX = deltaTouchX / Screen.width * 360;
+		else
+			deltaTouchX = -deltaTouchX / Screen.width * 360;
 
-			// Rotation direction reversed if touch is right of the player figure
-			if (touchZero.position.x > gameCamera.WorldToScreenPoint (playerPosition).x)
+		// Repeat for deltaY touch to enable circling movements to circle the map around
+		float deltaTouchY = touchZero.deltaPosition.y;
 
-				// Adjust for different device's screen widths. Then multiply by the angles that a total screen width swipe will rotate
-				deltaTouchY = deltaTouchY / Screen.height * 180;
-			else
-				deltaTouchY = -deltaTouchY / Screen.height * 180;
+		// Rotation direction reversed if touch is right of the player figure
+		if (touchZero.position.x > gameCamera.WorldToScreenPoint (playerPosition).x)
 
-			// Combine both swipe directions into one final value
-			float finalRotation = deltaTouchX + deltaTouchY;
+			// Adjust for different device's screen widths. Then multiply by the angles that a total screen width swipe will rotate
+			deltaTouchY = deltaTouchY / Screen.height * 180;
+		else
+			deltaTouchY = -deltaTouchY / Screen.height * 180;
 
-			// Apply the rotation to the transform around the player
-			transform.RotateAround (playerPosition, Vector3.up, finalRotation);
+		// Combine both swipe directions into one final value
+		float finalRotation = deltaTouchX + deltaTouchY;
 
-			// Apply the rotation to the compass ui
-			compass.transform.localRotation = Quaternion.Euler(compass.transform.localRotation.eulerAngles + new Vector3(0.0f, 0.0f, finalRotation));
+		// Apply the rotation to the transform around the player
+		transform.RotateAround (playerPosition, Vector3.up, finalRotation);
 
-			break;
+		// Apply the rotation to the compass ui
+		compass.transform.localRotation = Quaternion.Euler (compass.transform.localRotation.eulerAngles + new Vector3 (0.0f, 0.0f, finalRotation));
+	}
 
-		case 0:
-			// Sanity check
-//			touchZero = null;
-//			touchOne = null;
-			break;
+	void CheckForTap() {
+		
+		// Create a rayCastHit object
+		RaycastHit hit = new RaycastHit ();
 
-		default:
-			// Sanity check
-//			touchZero = null;
-//			touchOne = null;
-			break;
+		// Look at each touch
+		for (int i = 0; i < Input.touchCount; i++) {
+
+			// If touch has just begun
+			if (Input.GetTouch (i).phase.Equals (TouchPhase.Began)) {
+
+				// Raycast from the touch position
+				Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (i).position);
+
+				// if hit
+				if (Physics.Raycast (ray, out hit, maxCaptureDistance)) {
+
+					if (IsDebug) {
+						GetComponentInParent<GPS> ().SetIsDebug (true);
+						GetComponentInParent<GPS> ().SetPlayerMovePoint (hit.transform.position);
+						Debug.DrawLine (Input.mousePosition, hit.transform.position, Color.red, 10000, false);
+					}
+					if (hit.transform.gameObject.GetComponent ("PocketPalParent")) {
+						
+						PocketPalParent hitPocketPal = (PocketPalParent)hit.transform.gameObject.GetComponent ("PocketPalParent");
+						Debug.Log (hitPocketPal.PocketPalID);
+
+						// Add to the player's inventory
+						player.GetComponent<PocketPalInventory>().AddPocketPal (hit.transform.gameObject);
+					}
+				}
+			}
 		}
 	}
 
--- Pocket Pals App 1/Assets/Scripts/PlayerTouch.cs
+++ Pocket Pals App 1/Assets/Scripts/PlayerTouch.cs
@@ -18,18 +18,45 @@ public class PlayerTouch : MonoBehaviour {
 
     // Update is called once per frame
     void Update()
-    {
+	{
+/*		
         //check if the screen is touched / clicked   
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(0)))
         {
-            //declare a variable of RaycastHit struct
-            RaycastHit hit;
-            //Create a Ray on the tapped / clicked position
-            Ray ray;
+			// Create a raycast
+			RaycastHit hit = new RaycastHit();
+
+			for (int i = 0; i < Input.touchCount; i++) {
+				
+				if (Input.GetTouch (i).phase.Equals (TouchPhase.Began)) {
+					
+					Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (i).position);
+
+					if (Physics.Raycast (ray, out hit)) {
+
+						if (IsDebug)
+						{
+							GetComponentInParent<GPS>().SetIsDebug(true);
+							GetComponentInParent<GPS>().SetPlayerMovePoint(hit.transform.position);
+							Debug.DrawLine(Input.mousePosition, hit.transform.position, Color.red);
+						}
+						if (hit.transform.gameObject.GetComponent("PocketPalParent"))
+						{
+							PocketPalParent pocketPal = (PocketPalParent)hit.transform.gameObject.GetComponent("PocketPalParent");
+							Debug.Log(pocketPal.PocketPalID);
+							inventory.AddPocketPal(hit.transform.gameObject);
+
+						}
+					}
+				}
+			}
+
+
             //for unity editor
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+  //          ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             //for touch device
 
+<<<<<<< Updated upstream
             //Check if the ray hits any collider
             if (Physics.Raycast(ray, out hit))
             {
@@ -46,6 +73,39 @@ public class PlayerTouch : MonoBehaviour {
                     inventory.AddPocketPal(hit.transform.gameObject);
                 }
             }
+=======
+/*
+ * 
+ * 			//check if the screen is touched / clicked   
+			if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(0)))
+			{
+				//declare a variable of RaycastHit struct
+				RaycastHit hit;
+				//Create a Ray on the tapped / clicked position
+				Ray ray;
+				//for unity editor
+				ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				//for touch device
+
+				//Check if the ray hits any collider
+				if (Physics.Raycast(ray, out hit))
+				{
+					if (IsDebug)
+					{
+						GetComponentInParent<GPS>().SetIsDebug(true);
+						GetComponentInParent<GPS>().SetPlayerMovePoint(hit.transform.position);
+						Debug.DrawLine(Input.mousePosition, hit.transform.position, Color.red);
+					}
+					if (hit.transform.gameObject.GetComponent("PocketPalParent"))
+					{
+						PocketPalParent pocketPal = (PocketPalParent)hit.transform.gameObject.GetComponent("PocketPalParent");
+						Debug.Log(pocketPal.PocketPalID);
+						inventory.AddPocketPal(hit.transform.gameObject);
+
+					}
+				}
+
         }
-    }
+		*/
+	}
 }
