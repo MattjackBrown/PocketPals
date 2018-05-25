befc6500bc996f5d43f2806e7c6f94eb6072c72a
tristanbarlow <tristanbarlowg@gmail.com>
2018-05-25 11:18:29 +0100

    Started work for For scene manager class.

---
+++ Pocket Pals App 1/Assets/Scripts/EnvironmentChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentChanger : MonoBehaviour
+{
+    EnvironmentChanger Instance { set; get; }
+
+    public GameObject[] playerPositions;
+
+    public GameObject[] scenes;
+
+    //Used to reset the camera back in posisition.
+    public GameObject player;
+    private GPS gps;
+
+    //Script Behaviours
+    
+	// Use this for initialization
+	void Start ()
+    {
+        Instance = this;
+        gps = player.GetComponent<GPS>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+
+   public void SceneInit(int index)
+    {
+        gps.currentMap.gameObject.SetActive(false);
+        gps.mainCamera.transform.position = playerPositions[index].transform.position;
+        scenes[index].SetActive(true);
+    }
+
+}
