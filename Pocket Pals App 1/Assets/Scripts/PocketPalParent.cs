869aacb415811b5fb2c63eea7e5123afbb54b40e
tristanbarlowg@gmail.com <Samandfrodo12>
2018-06-01 18:12:27 +0100

    Added new assets, removed old. VG And Spawning will be broken!

--- Pocket Pals App 1/Assets/Scripts/PocketPalParent.cs
+++ Pocket Pals App 1/Assets/Scripts/PocketPalParent.cs
@@ -14,13 +14,7 @@ public class PocketPalParent : MonoBehaviour
     //this will be the ID for each pocketpal this should be unique to each pocketpal and used to identify it for the inventory system. 
     public int PocketPalID = 0;
 
-    public Vector3 Rotation = new Vector3(0, 15, 0);
-
-    public Vector3 Scale = new Vector3(1, 1, 1);
-
-    public Vector3 Offset = new Vector3(0, 0, 0);
-
-    public Sprite boarder;
+    public Vector3 RotationPerSecond = new Vector3(0, 15, 0);
 
     public float maxSpawnExp = 1000.0f;
 
@@ -40,12 +34,6 @@ public class PocketPalParent : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        transform.position += Offset;
-        //Some of the models are very small this will allow you to scale them
-        transform.localScale = Vector3.Scale(transform.localScale, Scale);
-
-        boarder  = Instantiate(boarder);
-
 	}
 
     private void OnCollisionEnter(Collision collision)
@@ -57,7 +45,7 @@ public class PocketPalParent : MonoBehaviour
     void Update ()
     {
         // rotates the object on the x, y and z axis
-        transform.Rotate(Rotation * Time.deltaTime); 
+        transform.Rotate(RotationPerSecond * Time.deltaTime); 
     }
 
     public void Captured()
