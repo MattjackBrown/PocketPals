a00b57e52f362490b4e2f18140431c2806167b8a
tristanbarlow <tristanbarlowg@gmail.com>
2018-07-04 15:08:42 +0100

    Added new resource spot model with animation. currently doesnt do anything

--- Pocket Pals App 1/Assets/Scripts/ResourceSpotParent.cs
+++ Pocket Pals App 1/Assets/Scripts/ResourceSpotParent.cs
@@ -8,5 +8,24 @@ public class ResourceSpotParent : MonoBehaviour
     public Vector2 IngameLocation { set; get; }
     public Vector2 LatLonLocation { set; get; }
 
+    private Animator anim;
 
+    private void Start()
+    {
+        gameObject.transform.Rotate(new Vector3(0,Random.Range(0,360), 0));
+        anim = GetComponent<Animator>();
+    }
+
+    public void Clicked()
+    {
+        anim.SetBool("Clicked", true);
+        //To DO: Zoom in to the game object
+
+    }
+    public void Finished()
+    {
+        anim.SetBool("Clicked", false);
+        transform.GetChild(0).gameObject.SetActive(false);
+        //To DO: Zoom back out
+    }
 }
