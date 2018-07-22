d1f32e42cb75c984b3e13dd7e161da04d08bc640
Richard Steele <richvon2208@googlemail.com>
2018-07-22 20:41:06 +0100

    New minigame ui

--- Pocket Pals App 1/Assets/Scripts/UIAnimationManager.cs
+++ Pocket Pals App 1/Assets/Scripts/UIAnimationManager.cs
@@ -80,6 +80,7 @@ public class UIAnimationManager : MonoBehaviour {
 		canvasAnimator.SetBool ("minigameSuccess", false);
 		canvasAnimator.SetBool ("minigameFail", false);
 		canvasAnimator.SetBool ("minigameToMap", false);
+		canvasAnimator.SetBool ("cameraChosen", false);
 	}
 
 	public void MinigameAccepted () {
@@ -121,6 +122,7 @@ public class UIAnimationManager : MonoBehaviour {
 		canvasAnimator.SetBool ("minigameDeclined", false);
 		canvasAnimator.SetBool ("minigameSuccess", false);
 		canvasAnimator.SetBool ("minigameFail", false);
+		canvasAnimator.SetBool ("cameraChosen", false);
 	}
 
 	public void OpenJournal (bool show)
@@ -177,6 +179,11 @@ public class UIAnimationManager : MonoBehaviour {
 	public void LoadingBarFinished () {
 
 		canvasAnimator.SetBool ("loadingBarFinished", true);
+	}
+
+	public void CameraChosen () {
+
+		canvasAnimator.SetBool ("cameraChosen", true);
 	}
 
 	public void OpenMapMenu () {
