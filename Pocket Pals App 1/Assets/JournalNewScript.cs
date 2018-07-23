4a39cabafc6fba0393b220a14f09daf9ef79ed44
Richard Steele <richvon2208@googlemail.com>
2018-07-23 23:05:54 +0100

    Journal image import and start on script

---
+++ Pocket Pals App 1/Assets/JournalNewScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalNewScript : MonoBehaviour {
+
+	public GameObject mainCamera, cameraJournalPositionObject, cameraLookAtPositionObject, playerModel, playerJournalPositionObject;
+
+	Vector3 cameraJournalPosition, cameraLookAtPosition, playerJournalPosition;
+
+
+	void Start () {
+
+		cameraJournalPosition = cameraJournalPositionObject.transform.position;
+		cameraLookAtPosition = cameraLookAtPositionObject.transform.position;
+		playerJournalPosition = playerModel.transform.position;
+
+
+		// Temp to test
+		mainCamera.transform.position = cameraJournalPosition;
+		mainCamera.transform.LookAt (cameraLookAtPosition);
+		playerModel.transform.position = playerJournalPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Temp to test
+		mainCamera.transform.position = cameraJournalPosition;
+		mainCamera.transform.LookAt (cameraLookAtPosition);
+		playerModel.transform.position = playerJournalPosition;
+	}
+}
