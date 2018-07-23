44fe2d6bb2aade865ee21ff38210c61553d3c31d
Richard Steele <richvon2208@googlemail.com>
2018-07-23 23:20:02 +0100

    This is probably not the right offset but it might be

--- Pocket Pals App 1/Assets/JournalNewScript.cs
+++ Pocket Pals App 1/Assets/JournalNewScript.cs
@@ -8,26 +8,29 @@ public class JournalNewScript : MonoBehaviour {
 
 	Vector3 cameraJournalPosition, cameraLookAtPosition, playerJournalPosition;
 
+	// Much trial and error to find this. It's late, I don't know if my maths is right
+	Vector3 playerModelPositionOffset = new Vector3 (-1.68f, 12.54f, -10.6f);
+
 
 	void Start () {
 
 		cameraJournalPosition = cameraJournalPositionObject.transform.position;
 		cameraLookAtPosition = cameraLookAtPositionObject.transform.position;
-		playerJournalPosition = playerModel.transform.position;
+
+//		playerJournalPosition = playerModel.transform.position;
+		playerJournalPosition = cameraJournalPosition + playerModelPositionOffset;
 
 
 		// Temp to test
 		mainCamera.transform.position = cameraJournalPosition;
 		mainCamera.transform.LookAt (cameraLookAtPosition);
+
 		playerModel.transform.position = playerJournalPosition;
+		playerModel.transform.LookAt (cameraJournalPosition);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		// Temp to test
-		mainCamera.transform.position = cameraJournalPosition;
-		mainCamera.transform.LookAt (cameraLookAtPosition);
-		playerModel.transform.position = playerJournalPosition;
+		
 	}
 }
