9518fccd32ea235574ed7272fc0fd16b0f8a7675
Richard Steele <richvon2208@googlemail.com>
2018-07-23 02:38:11 +0100

    Imported all character customisation stuff, started controller script

---
+++ Pocket Pals App 1/Assets/CharacterCustomisation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCustomisation : MonoBehaviour {
+
+	public GameObject hairObject;
+	public List<GameObject> hairChoices;
+
+	public Material bagMaterial;
+	public List<Texture2D> bagChoices;
+
+	public Material shirtMaterial;
+	public List<Texture2D> shirtChoices;
+
+	public Material shortsMaterial;
+	public List<Texture2D> shortsChoices;
+
+	public Material skinMaterial;
+	public List<Texture2D> skinChoices;
+
+	public Material bootsMaterial;
+	public List<Texture2D> bootsChoices;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
