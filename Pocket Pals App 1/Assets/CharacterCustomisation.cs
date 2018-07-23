65e783dc2b63ee6b5535c143152d2d6185bbab07
Richard Steele <richvon2208@googlemail.com>
2018-07-23 02:51:29 +0100

    Setting up the charController a bit more

--- Pocket Pals App 1/Assets/CharacterCustomisation.cs
+++ Pocket Pals App 1/Assets/CharacterCustomisation.cs
@@ -6,21 +6,27 @@ public class CharacterCustomisation : MonoBehaviour {
 
 	public GameObject hairObject;
 	public List<GameObject> hairChoices;
+	int hairIndex = 0;
 
 	public Material bagMaterial;
 	public List<Texture2D> bagChoices;
+	int bagIndex = 0;
 
 	public Material shirtMaterial;
 	public List<Texture2D> shirtChoices;
+	int shirtIndex = 0;
 
 	public Material shortsMaterial;
 	public List<Texture2D> shortsChoices;
+	int shortsIndex = 0;
 
 	public Material skinMaterial;
 	public List<Texture2D> skinChoices;
+	int skinIndex = 0;
 
 	public Material bootsMaterial;
 	public List<Texture2D> bootsChoices;
+	int bootsIndex = 0;
 
 
 	// Use this for initialization
@@ -31,5 +37,59 @@ public class CharacterCustomisation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	// Hair
+	public void SeeNextHairChoice () {
+		
+	}
+
+	public void SeePreviousHairChoice () {
+
+	}
+
+	// Bag
+	public void SeeNextBagChoice () {
+		
+	}
+
+	public void SeePreviousBagChoice () {
+
+	}
+
+	// Shirt
+	public void SeeNextShirtChoice () {
+
+	}
+
+	public void SeePreviousShirtChoice () {
+
+	}
+
+	// Shorts
+	public void SeeNextShortsChoice () {
+
+	}
+
+	public void SeePreviousShortsChoice () {
+
+	}
+
+	// Skin
+	public void SeeNextSkinChoice () {
+
+	}
+
+	public void SeePreviousSkinChoice () {
+
+	}
+
+	// Boots
+	public void SeeNextBootsChoice () {
+
+	}
+
+	public void SeePreviousBootsChoice () {
+
 	}
 }
