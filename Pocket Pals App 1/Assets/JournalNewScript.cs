f686cfc70770987dd931bd1fd810d0a2181c6459
Richard Steele <richvon2208@googlemail.com>
2018-07-25 00:32:52 +0100

    Got the right transform for the customisation view

--- Pocket Pals App 1/Assets/JournalNewScript.cs
+++ Pocket Pals App 1/Assets/JournalNewScript.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class JournalNewScript : MonoBehaviour {
 
-	public GameObject mainCamera, cameraJournalPositionObject, cameraLookAtPositionObject, playerModel, playerJournalPositionObject;
+	public GameObject mainCamera, playerModel, cameraJournalPositionObject, cameraLookAtPositionObject;
 
 	Vector3 cameraJournalPosition, cameraLookAtPosition, playerJournalPosition;
 
 	// Much trial and error to find this. It's late, I don't know if my maths is right
-	Vector3 playerModelPositionOffset = new Vector3 (-1.68f, 12.54f, -10.6f);
+	Vector3 playerModelPositionOffset = new Vector3 (-1.82f, 3.85f, 0.0f);
 
 
 	void Start () {
@@ -17,20 +17,19 @@ public class JournalNewScript : MonoBehaviour {
 		cameraJournalPosition = cameraJournalPositionObject.transform.position;
 		cameraLookAtPosition = cameraLookAtPositionObject.transform.position;
 
-//		playerJournalPosition = playerModel.transform.position;
+		playerModel.SetActive (true);
 		playerJournalPosition = cameraJournalPosition + playerModelPositionOffset;
-
-
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
 		// Temp to test
 		mainCamera.transform.position = cameraJournalPosition;
 		mainCamera.transform.LookAt (cameraLookAtPosition);
 
 		playerModel.transform.position = playerJournalPosition;
 		playerModel.transform.LookAt (cameraJournalPosition);
-	}
-	
-	// Update is called once per frame
-	void Update () {
 		
 	}
 }
