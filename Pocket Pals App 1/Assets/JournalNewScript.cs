90d677890e20188bdd94a530de4fe63f77ea45bb
Richard Steele <richvon2208@googlemail.com>
2018-07-25 00:47:08 +0100

    Setting up the customisation script a bit more. Now sleep

--- Pocket Pals App 1/Assets/JournalNewScript.cs
+++ Pocket Pals App 1/Assets/JournalNewScript.cs
@@ -7,8 +7,9 @@ public class JournalNewScript : MonoBehaviour {
 	public GameObject mainCamera, playerModel, cameraJournalPositionObject, cameraLookAtPositionObject;
 
 	Vector3 cameraJournalPosition, cameraLookAtPosition, playerJournalPosition;
+	Transform cameraMapTransform;
 
-	// Much trial and error to find this. It's late, I don't know if my maths is right
+	// Much trial and error to find this
 	Vector3 playerModelPositionOffset = new Vector3 (-1.82f, 3.85f, 0.0f);
 
 
@@ -17,19 +18,43 @@ public class JournalNewScript : MonoBehaviour {
 		cameraJournalPosition = cameraJournalPositionObject.transform.position;
 		cameraLookAtPosition = cameraLookAtPositionObject.transform.position;
 
-		playerModel.SetActive (true);
 		playerJournalPosition = cameraJournalPosition + playerModelPositionOffset;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		// Temp to test
+
+	public void Init () {
+
+		// Record the camera position to return back to after the menu
+		cameraMapTransform = mainCamera.transform;
+
+		// Position all used assets
 		mainCamera.transform.position = cameraJournalPosition;
 		mainCamera.transform.LookAt (cameraLookAtPosition);
 
+		playerModel.SetActive (true);
 		playerModel.transform.position = playerJournalPosition;
 		playerModel.transform.LookAt (cameraJournalPosition);
+
+	}
+
+	public void ExitBackToMap () {
+
+		// Return the Camera
+		mainCamera.transform.SetPositionAndRotation (cameraMapTransform.position, mainCamera.transform.rotation);
+
+		// Deactivate the player model used for the menu
+		playerModel.SetActive (false);
+
+		// TODO Change the control scheme
+	}
+
+	void Update () {
+		
+		// TODO Lerp movement between inspect positions
+
+		// TODO Set up menus with buttons for the functions
+		// TODO Buttons trigger camMovement lerps - set up a controlScheme
+		// TODO controlScheme allows rotating player. (prefab.transform.position.z as on it's side)
+		// Message Triss when done to link up to server data, shop bought bool, grey out shop button
 		
 	}
 }
